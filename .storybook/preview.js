f613d21500c6b5c3fec8d398164c1e26bbe9825f
Charles <cdwood123@gmail.com>
2026-06-16 00:07:51 -0700

    add storybook config

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import '../src/styles/global.css'
+
+export default {
+  parameters: {
+    backgrounds: {
+      default: 'warm white',
+      values: [
+        { name: 'warm white', value: '#FAF9F7' },
+        { name: 'white',      value: '#FFFFFF' },
+        { name: 'dark',       value: '#1A1A2E' },
+      ],
+    },
+  },
+}
